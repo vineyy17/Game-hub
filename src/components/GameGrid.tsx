6904e515d8a621df439b64802b2df026b4e709b8
Divine Charles <officialkb17@gmail.com>
2023-08-23 16:33:33 +0100

    Displayed platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ function GameGrid() {
       {error && <Text>{error}</Text>}
 
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
         padding="10px"
         spacing={10}
       >
