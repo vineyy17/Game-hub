d70748423ed3c4ed0c028d790c42d64716799846
Divine Charles <officialkb17@gmail.com>
2023-08-26 14:04:13 +0100

    Fetched genres and platforms with react query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -19,7 +19,7 @@ function PlatformSelector({ onSelectPlatform, selectedPlatform }: Props) {
         {selectedPlatform?.name || 'Platforms'}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
