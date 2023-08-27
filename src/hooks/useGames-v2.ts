f6e147fd79e2f2c6adf0241d48c28ee40e792e0d
Divine Charles <officialkb17@gmail.com>
2023-08-27 01:52:20 +0100

    Fixed production errors

--- src/hooks/useGames-v2.ts
+++ src/hooks/useGames-v2.ts
@@ -1,35 +1,35 @@
-import { GameQuery } from '../App'
-import useData from '../hooks/useData-v1'
+// import { GameQuery } from '../App'
+// import useData from '../hooks/useData-v1'
 
-export interface Platform {
-  id: number
-  name: string
-  slug: string
-}
+// export interface Platform {
+//   id: number
+//   name: string
+//   slug: string
+// }
 
-export interface Game {
-  id: number
-  name: string
-  background_image: string
-  parent_platforms: { platform: Platform }[]
-  metacritic: number
-  rating_top: number
-}
+// export interface Game {
+//   id: number
+//   name: string
+//   background_image: string
+//   parent_platforms: { platform: Platform }[]
+//   metacritic: number
+//   rating_top: number
+// }
 
-export const useGames = (
-  // selectedGenre: Genre | null,
-  // selectedPlatform: Platform | null,
-  gameQuery: GameQuery,
-) =>
-  useData<Game>(
-    '/games',
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery],
-  )
+// export const useGames = (
+//   // selectedGenre: Genre | null,
+//   // selectedPlatform: Platform | null,
+//   gameQuery: GameQuery,
+// ) =>
+//   useData<Game>(
+//     '/games',
+//     {
+//       params: {
+//         genres: gameQuery.genre?.id,
+//         platforms: gameQuery.platform?.id,
+//         ordering: gameQuery.sortOrder,
+//         search: gameQuery.searchText,
+//       },
+//     },
+//     [gameQuery],
+//   )
--- src/hooks/useGenres-v1.ts
+++ src/hooks/useGenres-v1.ts
@@ -1,42 +1,42 @@
-import { useState, useEffect } from 'react'
-import apiClient from '../services/api-client'
-import { CanceledError } from 'axios'
+// import { useState, useEffect } from 'react'
+// import apiClient from '../services/api-client'
+// import { CanceledError } from 'axios'
 
-export interface Genre {
-  id: number
-  name: string
-}
+// export interface Genre {
+//   id: number
+//   name: string
+// }
 
-interface FetchGenresResponse {
-  count: number
-  results: Genre[]
-}
+// interface FetchGenresResponse {
+//   count: number
+//   results: Genre[]
+// }
 
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([])
-  const [error, setError] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+// const useGenres = () => {
+//   const [genres, setGenres] = useState<Genre[]>([])
+//   const [error, setError] = useState('')
+//   const [isLoading, setIsLoading] = useState(false)
 
-  useEffect(() => {
-    const controller = new AbortController()
+//   useEffect(() => {
+//     const controller = new AbortController()
 
-    setIsLoading(true)
-    apiClient
-      .get<FetchGenresResponse>('/genres', { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results)
-        setIsLoading(false)
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return
-        setError(err.message)
-        setIsLoading(false)
-      })
+//     setIsLoading(true)
+//     apiClient
+//       .get<FetchGenresResponse>('/genres', { signal: controller.signal })
+//       .then((res) => {
+//         setGenres(res.data.results)
+//         setIsLoading(false)
+//       })
+//       .catch((err) => {
+//         if (err instanceof CanceledError) return
+//         setError(err.message)
+//         setIsLoading(false)
+//       })
 
-    return () => controller.abort()
-  }, [])
+//     return () => controller.abort()
+//   }, [])
 
-  return { genres, error, isLoading }
-}
+//   return { genres, error, isLoading }
+// }
 
-export default useGenres
+// export default useGenres
