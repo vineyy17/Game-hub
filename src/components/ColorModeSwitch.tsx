fa0271d4fe71296616aa6358771e2ac847bb34b9
Divine Charles <officialkb17@gmail.com>
2023-08-28 14:12:38 +0100

    Fixed color mode switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ function ColorModeSwitch() {
         isChecked={colorMode === 'dark'}
         onChange={toggleColorMode}
       />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark</Text>
     </HStack>
   )
 }
